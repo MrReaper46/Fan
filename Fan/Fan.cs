5f138da505b2da59be93318f35a42c35de5f4c71
Van Lam <lamvanr@gmail.com>
2023-05-04 18:11:18 +0700

    Add project files.

---
+++ Fan/Fan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    public class Fan
+    {
+        private const int _slow = 1;
+        private const int _medium = 2;
+        private const int _fast = 3;
+
+        private int _speed = _slow;
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+        //private bool on = false;
+        //private double radius = 5;
+        //private string color = "blue";
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(_slow);
+        }
+
+    }
+}
---
+++ Fan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan1 = new Fan();
+            Console.WriteLine(fan1.Speed);
+            fan1.PrintInfo();
+        }
+    }
+}
